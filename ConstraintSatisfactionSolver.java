e40356ea64287d8cc971ee8e5dd69c017a49e2b4
Vinay Shasi <vshasi@charlotte.edu>
2024-11-22 19:47:15 -0500

    Renaming Files.

--- ConstraintSatisfactionSolver.java
+++ ConstraintSatisfactionSolver.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class CSPColoring {
+public class ConstraintSatisfactionSolver {
 
     public static void main(String[] args) {
 
